df3f83ac06d618b73ab583dd4fd83ad3b14107ed
MATEUS SANTANA DE OLIVEIRA <mateus_oliveira8@estudante.sesisenai.org.br>
2024-10-23 21:53:19 -0300

    criacao-tabela-sa

--- sa-code/fut-agenda-java/src/main/java/fut/agenda/fut_agenda/repositories/FutAgendaRepository.java
+++
@@ -1,9 +0,0 @@
-package fut.agenda.fut_agenda.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fut.agenda.fut_agenda.entities.UsuarioFutAgendaEntity;
-
-public interface FutAgendaRepository extends JpaRepository <UsuarioFutAgendaEntity, Long> {
-    
-}
